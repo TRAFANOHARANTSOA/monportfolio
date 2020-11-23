62d90e5661439b7ae93947a504424aaa37f16836
Tix-learn-dev <t.rafanoharantsoa@codeur.online>
2020-11-23 09:15:09 +0100

    modif sur page skills

---
+++ skills.php
@@ -0,0 +1,35 @@
+<?php include 'header.php'; ?>
+<?php session_start();?>
+<section id="myskillspage" >
+  <!-- <div  class="myskills">
+    <ul class="myskillsicons">
+      <li class="allskills"><img src ="images/ps.png" class="iconps"/></li>
+      <li class="allskills"><img src ="images/ai.png" class="iconai"/></li>
+      <li class="allskills"><img src ="images/xd.png" class="iconxd"/></li>
+      <li class="allskills"><img src ="images/css.png" class="iconcss"/></li>
+      <li class="allskills"><img src ="images/html.png" class="iconhtml "/></li>
+      <li class="allskills"><img src ="images/js.png" class="iconjs"/></li>
+      <li class="allskills"><img src ="images/wp.png" class="iconwp"/></li>
+      <li class="allskills"><img src ="images/php.png" class="iconphp"/></li>
+      <li class="allskills"><img src ="images/mysql.png" class="iconmysql"/></li>
+    </ul>
+  </div> -->
+<div class="animface">
+  <div class="animfacewrap">
+    <img src="images/face1.png" alt="">
+    <img src="images/face2.png" alt="">
+    <img src="images/face3.png" alt="">
+    <img src="images/face5.png" alt="">
+  </div>
+</div>
+<div class="animface">
+  <div class="animfacewrap">
+    <img src="images/face1.png" alt="">
+    <img src="images/face2.png" alt="">
+    <img src="images/face3.png" alt="">
+    <img src="images/face5.png" alt="">
+  </div>
+</div>
+</section>
+
+<?php include 'footer.php' ?>

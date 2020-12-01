6e9cc86e23c089e4d71757a5f0b273f53a05595e
TRAFANOHARANTSOA <t.rafanoharansoa@codeur.online>
2020-12-01 09:01:54 +0100

    modif:ajout menu skill

--- skills.php
+++ skills.php
@@ -21,16 +21,107 @@
     <img src="images/face3.png" alt="">
     <img src="images/face5.png" alt="">
   </div>
-  <div class="animfacewrap">
-    <img src="images/face1.png" alt="">
-    <img src="images/face2.png" alt="">
-    <img src="images/face3.png" alt="">
-    <img src="images/face5.png" alt="">
+</div>
+<div class="skillsdomain">
+<div id="frontend">
+<h1 id="titlefrontend" onmouseover="openFrontendSkillList()" onmouseout="closeFrontendSkillList()">FRONT-END</h1>
+
+    <div id="frontendskilllist">
+    <hr class="rupture1">
+    <ul>
+      <li>Maquettage, wireframing</li>
+      <li>Réalisation de pages Web : HTML,CSS,JAVASCRIPT,Bootstrap</li>
+      <li>Développement d’applications Web</li>
+      <li>Gestion de la sécurité / accessibilité / ergonomie / responsivité</li>
+    </ul>
+    <div class="animface">
+      <div class="animfacewrap">
+        <img class="skillsicons" src="images/ps.png" alt="">
+      </div>
+    </div>
+    <div class="animface">
+      <div class="animfacewrap">
+        <img class="skillsicons" src="images/ai.png" alt="">
+      </div>
+    </div>
+    <div class="animface">
+      <div class="animfacewrap">
+        <img class="skillsicons" src="images/xd.png" alt="">
+      </div>
+    </div>
+    <div class="animface">
+      <div class="animfacewrap">
+        <img class="skillsicons" src="images/css.png" alt="">
+      </div>
+    </div>
+    <div class="animface">
+      <div class="animfacewrap">
+        <img class="skillsicons" src="images/html.png" alt="">
+      </div>
+    </div>
+    <div class="animface">
+      <div class="animfacewrap">
+        <img class="skillsicons" src="images/wp.png" alt="">
+      </div>
+    </div>
+    <div class="animface">
+      <div class="animfacewrap">
+        <img class="skillsicons" src="images/js.png" alt="">
+      </div>
+    </div>
+    </div>
+  </div>
+  <div id="backend">
+    <h1 onmouseover="openBackendSkillList()" onmouseout="closeBackendSkillList()">BACK-END</h1>
+    <div id="backendskilllist">
+    <ul>
+      <li>Conception & exploitation de bases de données relationnelle MySQL</li>
+      <li>Développement d'application Web en PHP / MySQL</li>
+      <li>Développement sécurisé et documenté d’API</li>
+      <li>Publication sur le web de sites et applications : FTP, SSH</li>
+    </ul>
+    </div>
+    <!-- <div class="animface">
+      <div class="animfacewrap">
+        <img class="skillsicons" src="images/php.png" alt="">
+      </div>
+    </div>
+    <div class="animface">
+      <div class="animfacewrap">
+        <img class="skillsicons" src="images/mysql.png" alt="">
+      </div>
+    </div>-->
+
+  </div>
+  <div id="Gestiondeprojet">
+    <h1 onmouseover="openProjectSkillList()" onmouseout="closeProjectSkillList()">GESTION DE PROJET</h1>
+    <div id="projectskilllist">
+      <ul>
+        <li>Travail collaboratif : GITHUB</li>
+        <li>Pilotage de projet : recueil des besoins, développement, livraison</li>
+      </ul>
+    </div>
+  </div>
+
+  <div id="formation">
+    <h1 onmouseover="openStudySkillList()" onmouseout="closeStudySkillList()">FORMATIONS</h1>
+    <div id="studyskilllist">
+      <ul>
+        <li>05/2020 - Formation Designer Web - Access Code School de Dijon </li>
+        <li>02/2020 - Dispositif en Amont de la Qualification (DAQ) Parcours à coloration numérique  - IRFA BFC </li>
+        <li>06/2010 - Diplôme Supérieur Spécialisé en Sciences Comptables  - INSCAE Madagascar </li>
+      </ul>
+    </div>
+  </div>
+  <div  id="experiences">
+    <h1 onmouseover="openExperienceSkillList()" onmouseout="closeExperienceSkillList()">EXPERIENCES</h1>
+    <div id="experienceskilllist">
+      <ul>
+        <li>ASSISTANT COMPTABLE</li>
+        <li>CONTRÔLEUR PERMANENT </li>
+      </ul>
+    </div>
   </div>
 </div>
-
-
-
 </section>
-
 <?php include 'footer.php' ?>
--- valid.js
+++ valid.js
@@ -1,3 +1,89 @@
+//PageSkills//
+const frontend = document.getElementById("frontend");
+const backend = document.getElementById("backend");
+const gestiondeprojet = document.getElementById("Gestiondeprojet");
+const formation = document.getElementById("formation");
+const experiences = document.getElementById("experiences");
+
+
+const frontendskilllist = document.getElementById("frontendskilllist");
+function openFrontendSkillList(){
+  frontendskilllist.style.visibility = "visible";
+  backend.style.visibility = "hidden";
+  gestiondeprojet.style.visibility = "hidden";
+  formation.style.visibility = "hidden";
+  experiences.style.visibility = "hidden";
+}
+function closeFrontendSkillList(){
+  frontendskilllist.style.visibility = "hidden";
+  backend.style.visibility = "visible";
+  gestiondeprojet.style.visibility = "visible";
+  formation.style.visibility = "visible";
+  experiences.style.visibility = "visible";
+}
+const backendskilllist = document.getElementById("backendskilllist");
+function openBackendSkillList(){
+  backendskilllist.style.visibility = "visible";
+  frontend.style.visibility = "hidden";
+  gestiondeprojet.style.visibility = "hidden";
+  formation.style.visibility = "hidden";
+  experiences.style.visibility = "hidden";
+}
+function closeBackendSkillList(){
+  backendskilllist.style.visibility = "hidden";
+  frontend.style.visibility = "visible";
+  gestiondeprojet.style.visibility = "visible";
+  formation.style.visibility = "visible";
+  experiences.style.visibility = "visible";
+}
+const projectskilllist = document.getElementById("projectskilllist");
+function openProjectSkillList(){
+  projectskilllist.style.visibility = "visible";
+  frontend.style.visibility = "hidden";
+  backend.style.visibility = "hidden";
+  formation.style.visibility = "hidden";
+  experiences.style.visibility = "hidden";
+
+}
+function closeProjectSkillList(){
+  projectskilllist.style.visibility = "hidden";
+  frontend.style.visibility = "visible";
+  backend.style.visibility = "visible";
+  formation.style.visibility = "visible";
+  experiences.style.visibility = "visible";
+}
+const studyskilllist = document.getElementById("studyskilllist");
+function openStudySkillList(){
+  studyskilllist.style.visibility = "visible";
+  frontend.style.visibility = "hidden";
+  backend.style.visibility = "hidden";
+  gestiondeprojet.style.visibility = "hidden";
+  experiences.style.visibility = "hidden";
+}
+function closeStudySkillList(){
+  studyskilllist.style.visibility = "hidden";
+  frontend.style.visibility = "visible";
+  backend.style.visibility = "visible";
+  gestiondeprojet.style.visibility = "visible";
+  experiences.style.visibility = "visible";
+}
+const experienceskilllist = document.getElementById("experienceskilllist");
+function openExperienceSkillList(){
+  experienceskilllist.style.visibility = "visible";
+  frontend.style.visibility = "hidden";
+  backend.style.visibility = "hidden";
+  gestiondeprojet.style.visibility = "hidden";
+  formation.style.visibility = "hidden";
+}
+function closeExperienceSkillList(){
+  experienceskilllist.style.visibility = "hidden";
+  frontend.style.visibility = "visible";
+  backend.style.visibility = "visible";
+  gestiondeprojet.style.visibility = "visible";
+  formation.style.visibility = "visible";
+}
+
+//Couleur des icones//
 const multipleId = document.querySelectorAll("contact,myskillspage");
 var linkcolor = document.querySelectorAll("#iconmenu1, #iconmenu2, #iconmenu3, #iconmenu4");
 
@@ -12,7 +98,7 @@ function disableHeaderText() {
   }
 }
 
-//CONTACT
+//CONTACT//
 // Il y a plusieurs façon de sélectionner un nœud DOM ; ici on récupère
 // le formulaire et le champ d'e-mail ainsi que l'élément span
 // dans lequel on placera le message d'erreur
